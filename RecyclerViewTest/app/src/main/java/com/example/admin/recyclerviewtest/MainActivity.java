b02da9390c13206508141e5a0374fe28934753d5
liudaoan <Liudaoan@xunlei.com>
2016-08-03 15:42:15 +0800

    create RecyclerViewTest

---
+++ RecyclerViewTest/app/src/main/java/com/example/admin/recyclerviewtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.recyclerviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
